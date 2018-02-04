bca1d2b72d51beb6e3c571820c181ef3c815c0d3
Caleb Hattingh <caleb.hattingh@gmail.com>
2018-02-04 21:06:51 +1000

    Generate foreign-key data.

--- pg_crud_gen.py
+++ pg_crud_gen.py
@@ -589,5 +589,48 @@ def entrypoint():
     loop.run_until_complete(main(args))
 
 
+class ForeignKey(NamedTuple):
+    constraint_name: str
+    source_schema: str
+    source_table: str
+    source_column: str
+    target_schema: str
+    target_table: str
+    target_column: str
+
+
+async def get_fk_data(connection: Connection) -> List[ForeignKey]:
+    sql = '''\
+    SELECT
+      o.conname AS constraint_name,
+      (SELECT nspname FROM pg_namespace WHERE oid=m.relnamespace) AS source_schema,
+      m.relname AS source_table,
+      (SELECT a.attname FROM pg_attribute a WHERE a.attrelid = m.oid AND a.attnum = o.conkey[1] AND a.attisdropped = false) AS source_column,
+      (SELECT nspname FROM pg_namespace WHERE oid=f.relnamespace) AS target_schema,
+      f.relname AS target_table,
+      (SELECT a.attname FROM pg_attribute a WHERE a.attrelid = f.oid AND a.attnum = o.confkey[1] AND a.attisdropped = false) AS target_column
+    FROM
+      pg_constraint o LEFT JOIN pg_class c ON c.oid = o.conrelid
+      LEFT JOIN pg_class f ON f.oid = o.confrelid LEFT JOIN pg_class m ON m.oid = o.conrelid
+    WHERE
+      o.contype = 'f' AND o.conrelid IN (SELECT oid FROM pg_class c WHERE c.relkind = 'r');
+    '''
+    records = await connection.fetch(sql)
+    results = []
+    for r in records:
+        results.append(
+            ForeignKey(
+                constraint_name=r['constraint_name'],
+                source_schema=r['source_schema'],
+                source_table=r['source_table'],
+                source_column=r['source_column'],
+                target_schema=r['target_schema'],
+                target_table=r['target_table'],
+                target_column=r['target_column']
+            )
+        )
+    return results
+
+
 if __name__ == '__main__':
     entrypoint()
